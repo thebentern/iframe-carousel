0b4c4b5e8bd0e4c1548c2b4a852ef9e069044ef5
Ben Meadors <thebentern@ben-mbp-2017.lindseynlr.local>
2017-11-30 09:51:18 -0600

    Handel errors

--- src/tests/index.js
+++ src/tests/index.js
@@ -20,7 +20,7 @@ describe('Main page', () => {
           expect($('h3')[1].children[0].data).to.equal('nettop')
           expect($('iframe')[0].attribs.src).to.equal('http://127.0.0.1:8080/')
           expect($('iframe')[1].attribs.src).to.equal('http://127.0.0.1:8081/')
-          done()
+          done(err)
         })
       })
     })
